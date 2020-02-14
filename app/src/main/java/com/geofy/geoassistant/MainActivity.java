d5002cd873c41e6e3253a59317ce2ed898b27788
Souro7 <sarkarsourobrata@gmail.com>
2020-02-14 17:35:01 +0530

    Add Sign up using Firebase authentication

--- app/src/main/java/com/geofy/geoassistant/MainActivity.java
+++ app/src/main/java/com/geofy/geoassistant/MainActivity.java
@@ -1,14 +1,100 @@
 package com.geofy.geoassistant;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailId, password;
+    Button btnSignUp;
+    TextView tvSignIn;
+    FirebaseAuth mAuth;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailId = findViewById(R.id.editText);
+        password = findViewById(R.id.editText3);
+        btnSignUp = findViewById(R.id.button2);
+        tvSignIn = findViewById(R.id.textView);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString();
+                String pwd = password.getText().toString();
+
+                if(email.isEmpty()) {
+                    emailId.setError("Please enter email id");
+                    emailId.requestFocus();
+                }
+                else if (pwd.isEmpty()) {
+                    password.setError("Please enter password");
+                    password.requestFocus();
+                }
+                else if (email.isEmpty() && pwd.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Fields are empty!", Toast.LENGTH_SHORT).show();
+                }
+                else if (!(email.isEmpty() && pwd.isEmpty())) {
+                    mAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isComplete()) {
+                                Toast.makeText(MainActivity.this, "Error creating user, please try again.", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                            }
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Error occured!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        // do stuff if user is already logged in
+    }
+
 }
